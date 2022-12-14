9a9ab76502036e9e8696e6040714d11c6334893b
Staninna <26458805+Staninna@users.noreply.github.com>
2022-12-14 13:50:06 +0100

    Removed warnings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 // This is the testing file for the project (temporary)
+#![allow(
+    dead_code,
+    unused_variables,
+    unused_imports,
+    unused_mut,
+    unused_assignments
+)]
+
 use sixfiveohtwo::processor::Processor;
 fn main() {
     let mut processor = Processor::new(0x00FF);
