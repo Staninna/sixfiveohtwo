d4da48b0e0918b5fb1ce1d50423769fc872212c4
Staninna <26458805+Staninna@users.noreply.github.com>
2022-12-16 21:14:28 +0100

    removed unused crate rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-// Set crate rules
-// #![allow(
-//     dead_code,
-//     unused_imports,
-//     unused_variables,
-//     unused_mut,
-//     unused_assignments
-// )]
-
 pub mod memory;
 pub mod opcodes;
 pub mod processor;
