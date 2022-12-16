fcd4c51518cc22ff920b4f0c364859fd64285d96
Staninna <26458805+Staninna@users.noreply.github.com>
2022-12-16 18:06:54 +0100

    Compacted `opcodes.rs`

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,28 +1,15 @@
 // Opcodes
 
 // Load/store instructions
-// LDA - Load Accumulator
-// LDX - Load X Register
-// LDY - Load Y Register
-// STA - Store Accumulator
-// STX - Store X Register
-// STY - Store Y Register
-
-#![allow(dead_code)] // TODO: Remove this when all unused opcodes are implemented
-pub const LDA: u8 = 0xA9;
-pub const LDX: u8 = 0xA2;
-pub const LDY: u8 = 0xA0;
-pub const STA: u8 = 0x8D;
-pub const STX: u8 = 0x8E;
-pub const STY: u8 = 0x8C;
+pub const LDA: u8 = 0xA9; // Load Accumulator
+pub const LDX: u8 = 0xA2; // Load X
+pub const LDY: u8 = 0xA0; // Load Y
+pub const STA: u8 = 0x8D; // Store Accumulator
+pub const STX: u8 = 0x8E; // Store X
+pub const STY: u8 = 0x8C; // Store Y
 
 // Register transfer instructions
-// TAX - Transfer Accumulator to X
-// TAY - Transfer Accumulator to Y
-// TXA - Transfer X to Accumulator
-// TYA - Transfer Y to Accumulator
-
-pub const TAX: u8 = 0xAA;
-pub const TAY: u8 = 0xA8;
-pub const TXA: u8 = 0x8A;
-pub const TYA: u8 = 0x98;
+pub const TAX: u8 = 0xAA; // Transfer Accumulator to X
+pub const TAY: u8 = 0xA8; // Transfer Accumulator to Y
+pub const TXA: u8 = 0x8A; // Transfer X to Accumulator
+pub const TYA: u8 = 0x98; // Transfer Y to Accumulator
